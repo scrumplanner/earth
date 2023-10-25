0905b880349e393ade942941ccf8bfddb5e56cbe
Serhat Koçhan <serhatkochan@hotmail.com.tr>
2023-10-25 19:13:41 +0300

    next-themes, formik, yup, zustand

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import DarkModeButton from "@/components/darkModeButton";
+
+export default function Home() {
+    return (
+        <>
+            <h1 className="text-4xl w-full bg-blue-700 p-3 flex justify-center">
+                Template
+            </h1>
+            <DarkModeButton />
+        </>
+    )
+}
---
+++ tailwind.config.ts
@@ -0,0 +1,16 @@
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  darkMode: 'class',
+  purge: ['./src/**/*.tsx'],
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+    },
+  },
+}
+export default config
